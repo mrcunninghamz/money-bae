b22d1fb65c319dee010441fcd1519bd0f0e2aa05
Karl Merecido <kmerecido@ostusa.com>
2026-09-03 10:23:25 -0500

    Add CDK app for API Gateway infra (ECR + App Runner)

---
+++ servers/api/infrastructure/cdk/lib/api-stack.ts
@@ -0,0 +1,25 @@
+import * as cdk from 'aws-cdk-lib';
+import * as ecr from 'aws-cdk-lib/aws-ecr';
+import * as secretsmanager from 'aws-cdk-lib/aws-secretsmanager';
+import * as apprunner from '@aws-cdk/aws-apprunner-alpha';
+import { Construct } from 'constructs';
+
+export class ApiStack extends cdk.Stack {
+  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+    super(scope, id, props);
+
+    const repo = new ecr.Repository(this, 'ApiRepo', { repositoryName: 'money-bae-api' });
+    const dbSecret = secretsmanager.Secret.fromSecretNameV2(this, 'DbSecret', 'money-bae-api/database-url');
+
+    new apprunner.Service(this, 'ApiService', {
+      source: apprunner.Source.fromEcr({
+        repository: repo,
+        tagOrDigest: 'latest',
+        imageConfiguration: {
+          environmentSecrets: { DATABASE_URL: apprunner.Secret.fromSecretsManager(dbSecret) },
+        },
+      }),
+      healthCheck: apprunner.HealthCheck.http({ path: '/health' }),
+    });
+  }
+}
